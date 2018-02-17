6f91fe87c03cc3f45611d01332054015625045f7
Tapiwa Musasa <tapiwa.musasa@blackmores.com.au>
2018-02-17 22:14:35 +1100

    Initial template

---
+++ src/AngTapsInventory.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tapiwa Musasa && Ang Li
+ *
+ */
+public class AngTapsInventory {
+
+	/**
+	 * 
+	 */
+	public AngTapsInventory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
